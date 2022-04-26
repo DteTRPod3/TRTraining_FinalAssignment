527ff923a310a9e58268c6da0433b1527310dd75
Atrey <vatrey@deloitte.com>
2022-04-26 14:08:05 +0530

    Added state Management in Landing Page which connects with Carousel, FeaturedCars and Progress Bar

--- src/pages/LandingPage/HomePage.tsx
+++ src/pages/LandingPage/HomePage.tsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import BackgroundCarousal from "../../components/Carousal/BackgroundCarousal";
 import CarTypeBar from "../../components/CarTypeBar/CarTypeBar";
 import FeatureCars from "../../components/FeatureCars/FeaturedCars";
 import Searchbar from "../../components/Searchbar/Searchbar";
+import { CarDetails } from "../../models/CarDetails";
+import { RootState } from "../../redux/configureStore";
+import { getCars } from "../../redux/store/cars/actions";
+import "./HomePage.scss"
 
 function HomePage() {
-  const [carType, setCarType] = useState("");
-  let activeType = "";
+  
+  const dispatch = useDispatch();
+  const [carType, setCarType] = useState("sedan");
+
   const dispatchCarsByType = (carType: string) => {
     if (carType === "sedan") {
       setCarType("sedan");
@@ -17,16 +24,30 @@ function HomePage() {
     } else {
       setCarType("");
     }
-    console.log(activeType);
   };
+
+  useEffect(() => {
+    dispatch(getCars(carType));
+  }, [carType]);
+
+  const cars: CarDetails[] = useSelector(
+      (state: RootState) => JSON.parse(JSON.stringify(state.cars)).cars
+    );
+
   return (
     <>
       <CarTypeBar carType={carType} dispatchCarsByType={dispatchCarsByType} />
-      <BackgroundCarousal />
-      <Searchbar />
-      <FeatureCars />
+      <div className="carasouel">
+        <BackgroundCarousal carType={carType} dispatchCarsByType={dispatchCarsByType} />
+        <Searchbar />
+      </div>
+      <FeatureCars featuredCars={cars}/>
     </>
   );
 }
 
 export default HomePage;
+function dispatch(arg0: { type: string; carType: string; }) {
+  throw new Error("Function not implemented.");
+}
+
