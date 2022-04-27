62ea2d4bda7afea6b3c29b18f6dee3c7a61e2e80
Atrey <vatrey@deloitte.com>
2022-04-27 16:53:51 +0530

    removed CarTypeBar test  comments

--- src/components/CarTypeBar/CarTypeBar.test.tsx
+++ src/components/CarTypeBar/CarTypeBar.test.tsx
@@ -2,14 +2,6 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CarTypeBar from "./CarTypeBar";
 
-// const CarTypeBar = () => {
-//   return (
-//     <BrowserRouter>
-//       <CarTypeBar />
-//     </BrowserRouter>
-//   );
-// };
-
 describe("CarTypeBar", () => {
   const dispatchCarsByType = jest.fn();
   it("renders the component", () => {
