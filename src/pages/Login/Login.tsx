90a47826295271addf5b463e4a743dca44ccbf58
Kartik Boodi <kboodi@deloitte.com>
2022-05-05 14:36:51 +0530

    Form validation

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,34 +1,143 @@
-import React, { useEffect } from "react";
-import { Form, Nav } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import "./Login.scss";
+import { Button, Form, Nav } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { contactpattern, emailpattern } from "../../constants";
 
 function Login() {
+  const [tabState, SetTabState] = useState("with-email");
+  const [userMainField, setuserMainField] = useState("Email address");
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onSubmit",
+  });
+
   useEffect(() => {
     document.title = "Xtreme Cars | Login Page";
   }, []);
+
+  const handleTabSwitch = (selectedKey: any) => {
+    SetTabState(`${selectedKey}`);
+  };
+
+  useEffect(() => {
+    if (tabState === "with-email") {
+      setuserMainField("Email address");
+    } else {
+      setuserMainField("Mobile Number");
+    }
+  }, [tabState]);
+
+  const onFormSubmit = () => {
+    navigate("/home");
+  };
+
   return (
     <div className="login-div">
-      <Form>
-        <Nav variant="tabs" fill>
+      <h5>Login To Xtreame Cars</h5>
+      <br />
+      <Form className="login-form" onSubmit={handleSubmit(onFormSubmit)}>
+        <Nav
+          variant="tabs"
+          defaultActiveKey="with-email"
+          activeKey={tabState}
+          onSelect={handleTabSwitch}
+        >
           <Nav.Item>
-            <Nav.Link>Login with Emial</Nav.Link>
+            <Nav.Link eventKey="with-email">Login with Emial</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link>Login with Mobile Number</Nav.Link>
+            <Nav.Link eventKey="with-mobile">Login with Mobile Number</Nav.Link>
           </Nav.Item>
         </Nav>
-        <Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+        <br />
 
+        <Form.Group>
+          {tabState === "with-email" && (
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>{userMainField}</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: emailpattern,
+                    message: "Invalid email address",
+                  },
+                })}
+              />
+              {errors.email && (
+                <p className="text-danger" data-testid="name-error">
+                  {errors.email.message}
+                </p>
+              )}
+            </Form.Group>
+          )}
+          {tabState === "with-mobile" && (
+            <Form.Group className="mb-3" controlId="formBasicMobile">
+              <Form.Label>Mobile Numbar</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter mobile number"
+                pattern={contactpattern}
+                {...register("mobile", {
+                  required: "Mobile number is required",
+                  minLength: {
+                    value: 10,
+                    message: "Mobile number cannot be less than 10 digits",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "Mobile number cannot be more than 10 digits",
+                  },
+                  pattern: {
+                    value: /^[6-9]\d{9}$/,
+                    message: "Invalid Mobile number",
+                  },
+                })}
+              />
+              {errors.mobile && (
+                <p className="text-danger" data-testid="name-error">
+                  {errors.mobile.message}
+                </p>
+              )}
+            </Form.Group>
+          )}
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              {...register("password", { required: "Password is rquired" })}
+            />
+            {errors.password && (
+              <p className="text-danger" data-testid="name-error">
+                {errors.password.message}
+              </p>
+            )}
           </Form.Group>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="linkToSignup">
+          <Link to="/" className="register-link">
+            New User? Register now
+          </Link>
+        </Form.Group>
+        <br />
+        <Form.Group className="mb-3" controlId="buttonGrp">
+          <Button variant="info" type="submit">
+            Login
+          </Button>
+          <Button variant="light" type="button">
+            Reset
+          </Button>
         </Form.Group>
       </Form>
     </div>
