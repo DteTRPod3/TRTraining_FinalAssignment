8ce1592e61bf5aa2490d0342f0507d28a697c17e
pant-manas <manapant@deloitte.com>
2022-05-06 10:57:47 +0530

    Validations added for signup form

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,7 +1,21 @@
-import { Button, Container, Form, Card, Modal, FormGroup } from "react-bootstrap";
+import {
+  Button,
+  Container,
+  Form,
+  Card,
+  Modal,
+  FormGroup,
+} from "react-bootstrap";
 import React, { useEffect } from "react";
 import "./SignUp.scss";
-import { capitalLetterPattern, contactpattern, emailpattern, lowerLetterPattern, numbersPattern, specialCharacterPattern} from "../../constants";
+import {
+  capitalLetterPattern,
+  contactpattern,
+  emailpattern,
+  lowerLetterPattern,
+  numbersPattern,
+  specialCharacterPattern,
+} from "../../constants";
 import { useForm, useFormState } from "react-hook-form";
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -13,33 +27,30 @@ function SignUp() {
     window.scrollTo(0, 0);
   }, [location]);
 
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     control,
-    getValues,
+    watch,
     formState: { errors },
   } = useForm({
     mode: "onChange",
-    reValidateMode: 'onChange',
-    criteriaMode: "all"
+    reValidateMode: "onChange",
+    criteriaMode: "all",
   });
 
-  const {dirtyFields} = useFormState({
-    control
-  })
+  const { dirtyFields } = useFormState({
+    control,
+  });
+  const watchPassword = watch("password")
 
   useEffect(() => {
     document.title = "Xtreme Cars | Sign Up";
   }, []);
-
   const onSubmit = (formData: any) => {
-    console.log(formData);
     toast((t) => (
       <div
-        // style={{ transition: "all 2.5s ease-out", opacity: t.visible ? 1 : 0 }}
         className="toast-signup"
       >
         <span>Profile Created Successfully</span>
@@ -59,114 +70,214 @@ function SignUp() {
       <Container>
         <h3>Sign Up</h3>
         <Card className="form-content">
-            <Form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-              <Form.Group className="mb-3 form-group-container" controlId="formGroupName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter name..."
-                  {...register("name", { required: "Name is required" })}
-                />
-                {errors.name && <p className="text-danger" data-testid="name-error">{errors.name.message}</p>}
-              </Form.Group>
-              <Form.Group className="mb-3 form-group-container" controlId="formGroupContact">
-                <Form.Label>Mobile Number</Form.Label>
-                <Form.Control
-                  type="tel"
-                  placeholder="Enter contact..."
-                  {...register("contact", {
-                    required: "Mobile Number is required",
-                    minLength: {
-                      value: 10,
-                      message: "Mobile number cannot be less than 10 digits",
-                    },
-                    maxLength: {
-                      value: 10,
-                      message: "Mobile number cannot be more than 10 digits",
-                    },
-                  })}
-                />
-                {errors.contact && <p className="text-danger" data-testid="contact-error">{errors.contact.message}</p>}
-              </Form.Group>
-              <Form.Group className="mb-3 form-group-container" controlId="formGroupAddress">
-                <Form.Label>Address</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter address..."
-                  {...register("address")}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3 form-group-container" controlId="formGroupPincode">
-                <Form.Label>PinCode</Form.Label>
-                <Form.Control
-                  type="number"
-                  placeholder="Enter pincode..."
-                  {...register("pincode", { 
-                    required: "Pincode is required",
-                    minLength:{
-                      value:6,
-                      message:"pin code cannot be less than 6 digits"
-                    },
-                    maxLength:{
-                      value:6,
-                      message:"pin code cannot be more than 6 digits"
-                    }
-                 })}
-                />
-                {errors.pincode && <p className="text-danger" data-testid="address-error">{errors.pincode.message}</p>}
-              </Form.Group>
-              <Form.Group className="mb-3 form-group-container" controlId="formGroupEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email..."
-                  {...register("email", {
-                    required: "Email is required",
-                    pattern: {
-                      value: emailpattern,
-                      message: "This is not a valid email",
-                    },
-                  })}
-                />
-                {errors.email && <p className="text-danger" data-testid="email-error">{errors.email.message}</p>}
-              </Form.Group>
-              <Form.Group className="mb-3 form-group-container" controlId="formGroupPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Enter password..."
-                  {...register("password", {
-                    validate:{
-                      lowercase:value=>lowerLetterPattern.test(value) || "The password must contain one lower case letter",
-                      uppercase:value=>capitalLetterPattern.test(value) || "The password must contain one upper case letter",
-                      minlength:value=>value.length>=8 || "The password length must be atleast 8",
-                      specialCharacter:value=>specialCharacterPattern.test(value) || `The password must contain a special character (! @ # $ % & ? / : ; . , " ')`,
-                      numbers:value=>numbersPattern.test(value) || "The password must contain a number",
-
-                    }
-                  })}
-                />
-                {errors?.password?.types["lowercase"] && <p className="text-danger" data-testid="address-error">{errors?.password?.types["lowercase"]}</p>}
-                {errors?.password?.types["lowercase"] && <p className="text-success" data-testid="address-error">{errors?.password?.types["lowercase"]}</p>}
-                {errors?.password?.types["uppercase"] && <p className="text-danger" data-testid="address-error">{errors?.password?.types["uppercase"]}</p>}
-                {errors?.password?.types["minlength"] && <p className="text-danger" data-testid="address-error">{errors?.password?.types["minlength"]}</p>}
-                {errors?.password?.types["numbers"] && <p className="text-danger" data-testid="address-error">{errors?.password?.types["numbers"]}</p>}
-                {errors?.password?.types["specialCharacter"] && <p className="text-danger" data-testid="address-error">{errors?.password?.types["specialCharacter"]}</p>}
-              </Form.Group>
-              <Form.Group className="mb-3 form-group-container" controlId="formGroupConfirmPassword">
-                <Form.Label>Re-enter Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Re-enter the password"
-                  {...register("confirmpassword", { 
-                    required: "Please re-enter the password",
-                    validate:  value=>value===getValues("password") || "Password does not match" 
-                  })}
-                />
-                {errors.confirmpassword && <p className="text-danger" data-testid="address-error">{errors.confirmpassword.message}</p>}
-                {!errors.confirmpassword && dirtyFields["password"] && dirtyFields["confirmpassword"] && <p className="text-success">Tick</p>}
-              </Form.Group>
-              <FormGroup className="form-group-container">
+          <Form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+            <Form.Group
+              className="mb-3 form-group-container"
+              controlId="formGroupName"
+            >
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name..."
+                {...register("name", { required: "Name is required" })}
+              />
+              {errors.name && (
+                <p className="text-danger" data-testid="name-error">
+                  {errors.name.message}
+                </p>
+              )}
+            </Form.Group>
+            <Form.Group
+              className="mb-3 form-group-container"
+              controlId="formGroupContact"
+            >
+              <Form.Label>Mobile Number</Form.Label>
+              <Form.Control
+                type="tel"
+                placeholder="Enter contact..."
+                {...register("contact", {
+                  required: "Mobile Number is required",
+                  minLength: {
+                    value: 10,
+                    message: "Mobile number cannot be less than 10 digits",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "Mobile number cannot be more than 10 digits",
+                  },
+                })}
+              />
+              {errors.contact && (
+                <p className="text-danger" data-testid="contact-error">
+                  {errors.contact.message}
+                </p>
+              )}
+            </Form.Group>
+            <Form.Group
+              className="mb-3 form-group-container"
+              controlId="formGroupAddress"
+            >
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter address..."
+                {...register("address")}
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3 form-group-container"
+              controlId="formGroupPincode"
+            >
+              <Form.Label>PinCode</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter pincode..."
+                {...register("pincode", {
+                  required: "Pincode is required",
+                  minLength: {
+                    value: 6,
+                    message: "pin code cannot be less than 6 digits",
+                  },
+                  maxLength: {
+                    value: 6,
+                    message: "pin code cannot be more than 6 digits",
+                  },
+                })}
+              />
+              {errors.pincode && (
+                <p className="text-danger" data-testid="pincode-error">
+                  {errors.pincode.message}
+                </p>
+              )}
+            </Form.Group>
+            <Form.Group
+              className="mb-3 form-group-container"
+              controlId="formGroupEmail"
+            >
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email..."
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: emailpattern,
+                    message: "This is not a valid email",
+                  },
+                })}
+              />
+              {errors.email && (
+                <p className="text-danger" data-testid="email-error">
+                  {errors.email.message}
+                </p>
+              )}
+            </Form.Group>
+            <Form.Group
+              className="mb-3 form-group-container"
+              controlId="formGroupPassword"
+            >
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password..."
+                {...register("password", {
+                  validate: {
+                    lowercase: (value) =>
+                      lowerLetterPattern.test(value) ||
+                      "The password must contain one lower case letter",
+                    uppercase: (value) =>
+                      capitalLetterPattern.test(value) ||
+                      "The password must contain one upper case letter",
+                    minlength: (value) =>
+                      value.length >= 8 ||
+                      "The password length must be atleast 8",
+                    specialCharacter: (value) =>
+                      specialCharacterPattern.test(value) ||
+                      `The password must contain a special character (! @ # $ % & ? / : ; . , " ')`,
+                    numbers: (value) =>
+                      numbersPattern.test(value) ||
+                      "The password must contain a number",
+                  },
+                })}
+              />
+              {errors?.password?.types["lowercase"] && (
+                <p className="text-danger" data-testid="lowercase-error">
+                  {errors?.password?.types["lowercase"]}
+                </p>
+              )}
+              {!errors?.password?.types["lowercase"] && dirtyFields["password"] && (
+                <p className="text-success" data-testid="password-success">
+                  The password must contain one lower case letter
+                </p>
+              )}
+              {errors?.password?.types["uppercase"] && (
+                <p className="text-danger" data-testid="uppercase-error">
+                  {errors?.password?.types["uppercase"]}
+                </p>
+              )}
+              {!errors?.password?.types["uppercase"] && dirtyFields["password"] && (
+                <p className="text-success" data-testid="password-success">
+                  The password must contain one upper case letter
+                </p>
+              )}
+              {errors?.password?.types["minlength"] && (
+                <p className="text-danger" data-testid="address-error">
+                  {errors?.password?.types["minlength"]}
+                </p>
+              )}
+              {!errors?.password?.types["minlength"] && dirtyFields["password"] && (
+                <p className="text-success" data-testid="password-success">
+                  The password length must be atleast 8
+                </p>
+              )}
+              {errors?.password?.types["numbers"] && (
+                <p className="text-danger" data-testid="address-error">
+                  {errors?.password?.types["numbers"]}
+                </p>
+              )}
+              {!errors?.password?.types["numbers"] && dirtyFields["password"] && (
+                <p className="text-success" data-testid="password-success">
+                  The password must contain a number
+                </p>
+              )}
+              {errors?.password?.types["specialCharacter"] && (
+                <p className="text-danger" data-testid="address-error">
+                  {errors?.password?.types["specialCharacter"]}
+                </p>
+              )}
+              {!errors?.password?.types["specialCharacter"] && dirtyFields["password"] && (
+                <p className="text-success" data-testid="password-success">
+                  The password must contain a special character (! @ # $ % & ? / : ; . , " ')
+                </p>
+              )}
+            </Form.Group>
+            <Form.Group
+              className="mb-3 form-group-container"
+              controlId="formGroupConfirmPassword"
+            >
+              <Form.Label>Re-enter Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Re-enter the password"
+                {...register("confirmpassword", {
+                  required: "Please re-enter the password",
+                  validate: (value) =>
+                    value === watchPassword ||
+                    "Password does not match",
+                })}
+              />
+              {errors.confirmpassword && (
+                <p className="text-danger" data-testid="address-error">
+                  {errors.confirmpassword.message}
+                </p>
+              )}
+              {!errors.confirmpassword &&
+                dirtyFields["password"] &&
+                dirtyFields["confirmpassword"] && (
+                  <p className="text-success">Password match</p>
+                )}
+            </Form.Group>
+            <FormGroup className="form-group-container">
               <Button variant="danger" type="submit">
                 Submit
               </Button>
@@ -177,7 +288,7 @@ function SignUp() {
       <Toaster
         toastOptions={{
           duration: 2000,
-          position: "bottom-center",
+          position: "top-right",
           className: "toast-main",
         }}
       />
--- src/pages/SignUp/__test__/Signup.test.tsx
+++ src/pages/SignUp/__test__/Signup.test.tsx
@@ -13,16 +13,36 @@ describe("Sign Up Form", () => {
       </MemoryRouter>
     );
 
-    const inputName = screen.getByPlaceholderText(/enter name\.\.\./i);
-    const inputContact = screen.getByPlaceholderText(/enter contact\.\.\./i);
-    const inputAddress = screen.getByPlaceholderText(/enter address\.\.\./i);
-    const inputEmail = screen.getByPlaceholderText(/enter email\.\.\./i);
+    const inputName = screen.getByRole("textbox", {
+      name: /name/i,
+    });
+    const inputContact = screen.getByRole("textbox", {
+      name: /mobile number/i,
+    });
+    const inputAddress = screen.getByRole("textbox", {
+      name: /address/i,
+    });
+    const inputPincode = screen.getByRole("spinbutton", {
+      name: /pincode/i,
+    });
+    const inputEmail = screen.getByRole("textbox", {
+      name: /email/i,
+    });
+
+    const inputPassword = screen.getByPlaceholderText(/enter password\.\.\./i);
+    const inputConfirmPassword = screen.getByPlaceholderText(
+      /re\-enter the password/i
+    );
 
     expect(inputName).toBeInTheDocument();
     expect(inputContact).toBeInTheDocument();
     expect(inputAddress).toBeInTheDocument();
+    expect(inputPincode).toBeInTheDocument();
     expect(inputEmail).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
+    expect(inputConfirmPassword).toBeInTheDocument();
   });
+
   it("checks for valid value in name input field", () => {
     render(
       <MemoryRouter>
@@ -30,15 +50,20 @@ describe("Sign Up Form", () => {
       </MemoryRouter>
     );
 
-    const inputName = screen.getByPlaceholderText(/enter name\.\.\./i);
+    const inputName = screen.getByRole("textbox", {
+      name: /name/i,
+    });
     act(() => {
       userEvent.type(inputName, "Manas");
     });
 
-    expect(screen.getByPlaceholderText(/enter name\.\.\./i)).toHaveValue(
-      "Manas"
-    );
+    expect(
+      screen.getByRole("textbox", {
+        name: /name/i,
+      })
+    ).toHaveValue("Manas");
   });
+
   it("checks for valid value in contact input field", () => {
     render(
       <MemoryRouter>
@@ -46,15 +71,20 @@ describe("Sign Up Form", () => {
       </MemoryRouter>
     );
 
-    const inputContact = screen.getByPlaceholderText(/enter contact\.\.\./i);
+    const inputContact = screen.getByRole("textbox", {
+      name: /mobile number/i,
+    });
     act(() => {
       userEvent.type(inputContact, "9876543219");
     });
 
-    expect(screen.getByPlaceholderText(/enter contact\.\.\./i)).toHaveValue(
-      "9876543219"
-    );
+    expect(
+      screen.getByRole("textbox", {
+        name: /mobile number/i,
+      })
+    ).toHaveValue("9876543219");
   });
+
   it("checks for valid value in address input field", () => {
     render(
       <MemoryRouter>
@@ -62,14 +92,39 @@ describe("Sign Up Form", () => {
       </MemoryRouter>
     );
 
-    const inputAddress = screen.getByPlaceholderText(/enter address\.\.\./i);
+    const inputAddress = screen.getByRole("textbox", {
+      name: /address/i,
+    });
     act(() => {
       userEvent.type(inputAddress, "India");
     });
 
-    expect(screen.getByPlaceholderText(/enter address\.\.\./i)).toHaveValue(
-      "India"
+    expect(
+      screen.getByRole("textbox", {
+        name: /address/i,
+      })
+    ).toHaveValue("India");
+  });
+
+  it("checks for valid value in pincode input field", () => {
+    render(
+      <MemoryRouter>
+        <SignUp></SignUp>
+      </MemoryRouter>
     );
+
+    const inputPincode = screen.getByRole("spinbutton", {
+      name: /pincode/i,
+    });
+    act(() => {
+      userEvent.type(inputPincode, "123456");
+    });
+
+    expect(
+      screen.getByRole("spinbutton", {
+        name: /pincode/i,
+      })
+    ).toHaveValue(123456);
   });
 
   it("checks for valid value in email input field", () => {
@@ -79,14 +134,56 @@ describe("Sign Up Form", () => {
       </MemoryRouter>
     );
 
-    const inputEmail = screen.getByPlaceholderText(/enter email\.\.\./i);
+    const inputEmail = screen.getByRole("textbox", {
+      name: /email/i,
+    });
 
     act(() => {
       userEvent.type(inputEmail, "manas@gmail.com");
     });
 
-    expect(screen.getByPlaceholderText(/enter email\.\.\./i)).toHaveValue(
-      "manas@gmail.com"
+    expect(
+      screen.getByRole("textbox", {
+        name: /email/i,
+      })
+    ).toHaveValue("manas@gmail.com");
+  });
+
+  it("checks for valid value in password input field", () => {
+    render(
+      <MemoryRouter>
+        <SignUp></SignUp>
+      </MemoryRouter>
+    );
+
+    const inputPassword = screen.getByPlaceholderText(/enter password\.\.\./i);
+
+    act(() => {
+      userEvent.type(inputPassword, "Aasdasd12/@1");
+    });
+
+    expect(screen.getByPlaceholderText(/enter password\.\.\./i)).toHaveValue(
+      "Aasdasd12/@1"
+    );
+  });
+
+  it("checks for valid value in confirm password input field", () => {
+    render(
+      <MemoryRouter>
+        <SignUp></SignUp>
+      </MemoryRouter>
+    );
+
+    const inputConfirmPassword = screen.getByPlaceholderText(
+      /re\-enter the password/i
+    );
+
+    act(() => {
+      userEvent.type(inputConfirmPassword, "Aasdasd12/@1");
+    });
+
+    expect(screen.getByPlaceholderText(/re\-enter the password/i)).toHaveValue(
+      "Aasdasd12/@1"
     );
   });
 
@@ -156,7 +253,8 @@ describe("Sign Up Form", () => {
 
     expect(await screen.findAllByTestId("contact-error")).toHaveLength(1);
   });
-  it("checks for error message when address is not added", async () => {
+
+  it("checks for error message when pincode is not added", async () => {
     render(
       <MemoryRouter>
         <SignUp></SignUp>
@@ -187,9 +285,43 @@ describe("Sign Up Form", () => {
       })
     );
 
-    expect(await screen.findAllByTestId("address-error")).toHaveLength(1);
+    expect(await screen.findAllByTestId("pincode-error")).toHaveLength(1);
   });
+
   it("checks for error message when email is not added", async () => {
+    render(
+      <MemoryRouter>
+        <SignUp></SignUp>
+      </MemoryRouter>
+    );
+    fireEvent.input(screen.getByRole("textbox", { name: /name/i }), {
+      target: {
+        value: "Manas",
+      },
+    });
+
+    fireEvent.input(screen.getByRole("textbox", { name: /mobile number/i }), {
+      target: {
+        value: 9345678998,
+      },
+    });
+
+    fireEvent.input(screen.getByRole("textbox", { name: /address/i }), {
+      target: {
+        value: "address",
+      },
+    });
+
+    fireEvent.submit(
+      screen.getByRole("button", {
+        name: /submit/i,
+      })
+    );
+
+    expect(await screen.findAllByTestId("email-error")).toHaveLength(1);
+  });
+
+  it("checks for error message when password is not added", async () => {
     render(
       <MemoryRouter>
         <SignUp></SignUp>
